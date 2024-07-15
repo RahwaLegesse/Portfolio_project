5a91fdc6f9cd52b0a28b11ab30845ecf099cfcf8
EsayasA <esayasaregawi29@gmail.com>
2024-07-15 08:44:57 +0200

    adding product list to cart

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -4,8 +4,11 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Checkbox, Radio } from "antd";
 import { Prices } from "../components/Prices";
+import { useCart } from "../context/cart";
+import toast from "react-hot-toast";
 const HomePage = () => {
   const navigate = useNavigate();
+  const [cart, setCart] = useCart();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [checked, setChecked] = useState([]);
@@ -152,12 +155,22 @@ const HomePage = () => {
                   </p>
                   <p className="card-text"> $ {p.price}</p>
                   <button
-                    className="btn btn-primary"
+                    className="btn btn-primary ms-1"
                     onClick={() => navigate(`/product/${p.slug}`)}
                   >
                     More Details
                   </button>
-                  <button className="btn btn-secondary">
+                  <button
+                    className="btn btn-secondary ms-1"
+                    onClick={() => {
+                      setCart([...cart, p]);
+                      localStorage.setItem(
+                        "cart",
+                        JSON.stringify([...cart, p])
+                      );
+                      toast.success("Item Added to cart");
+                    }}
+                  >
                     ADD TO CART
                   </button>
                 </div>
